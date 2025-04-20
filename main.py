f40ace0eece69dc95bc47cbb90a9f63aaeec11b1
Narendra Annabathula <narendraannabathula@VIRA.local>
2025-04-20 11:47:25 -0700

    completed CH2:L6

--- main.py
+++ main.py
@@ -3,22 +3,19 @@ from player import Player
 from constants import *
 
 def main():
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    while True:
-        clock = pygame.time.Clock()
-        dt = 0
+    clock = pygame.time.Clock()
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    player = Player(x,y)
+    dt = 0
+    while True:     
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-
-        screen.fill(color=(0,0,0))
-        x = SCREEN_WIDTH / 2
-        y = SCREEN_HEIGHT / 2
-        player = Player(x,y)
+        player.update(dt)
+        screen.fill(color=(0,0,0))    
         player.draw(screen)
         pygame.display.flip()
         delta_milliseconds = clock.tick(60)

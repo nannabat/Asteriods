0420778fe82e1d891c193027cb6c2692065a8e8d
Narendra Annabathula <narendraannabathula@VIRA.local>
2025-04-20 10:15:53 -0700

    completed CH2:L5

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+from player import Player
 from constants import *
 
 def main():
@@ -8,12 +9,21 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     while True:
+        clock = pygame.time.Clock()
+        dt = 0
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
         screen.fill(color=(0,0,0))
+        x = SCREEN_WIDTH / 2
+        y = SCREEN_HEIGHT / 2
+        player = Player(x,y)
+        player.draw(screen)
         pygame.display.flip()
+        delta_milliseconds = clock.tick(60)
+        dt = delta_milliseconds / 1000
+
 
 
 if __name__ == "__main__":
